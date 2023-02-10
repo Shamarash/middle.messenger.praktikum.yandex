aefdc90a0f21ecbffcdca2d8935d68fce4e9b804
shamarash <shamarash@yandex.ru>
2023-02-10 11:54:36 +0300

    sprint_2

--- src/utils/Patterns.ts
+++ src/utils/Patterns.ts
@@ -1,4 +1,5 @@
-export const LoginPattern = '^[A-Za-z]\\w{3,20}$'
-export const NamePattern = '/^[A-ZА-Я][a-zа-я]*[-]?[a-zа-я]*$/gm'
+// eslint-disable-next-line no-useless-escape
+export const LoginPattern = '/^(?=.*[A-Za-z0-9]$)[A-Za-z][A-Za-z\d_-]{2,19}$/gm'
+export const NamePattern = '/^(?=.*[A-Za-zА-Яа-яЁё]$)[A-ZА-ЯЁ][A-Za-zА-Яа-яЁё_-]{1,}$/gm'
 export const PhonePattern = '/^+?d+$/gm'
-export const PasswordPattern = '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,40}$'
+export const PasswordPattern = '/^(?=.*?[A-ZА-ЯЁ])(?=.*?[a-zа-яё])(?=.*?[0-9]).{8,40}$/gm'
